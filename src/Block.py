7844b74aab384c75af871060fbe1940b0b4d76d6
sipetit <simon.petit@ecole.ensicaen.fr>
2020-12-13 16:19:56 +0100

    for Florian
    Signed-off-by: sipetit <simon.petit@ecole.ensicaen.fr>

--- src/Block.py
+++ src/Block.py
@@ -18,9 +18,8 @@ class Block:
 class BlockChain:
 
     def __init__(self):
-        self.blocks = []
+        self.blocks = self.init_DB()
         self.create_first_block()  # The first block doesn't have previous hash
-        self.number_of_block = 0
 
     def create_first_block(self):
         first_block = Block(0, "First Block", None, datetime.now())
@@ -28,5 +27,5 @@ class BlockChain:
         self.add_block(first_block)
 
     def add_block(self, block):
-        self.blocks[self.number_of_block] = block
-        self.number_of_block += 1
+        
+    def init_DB(self):
