f2dc1f80c60a6e0244e0fd6da540bd63af47adec
sipetit <simon.petit@ecole.ensicaen.fr>
2020-12-13 16:00:26 +0100

    Begin

---
+++ Block.py
@@ -0,0 +1,32 @@
+from hashlib import sha256
+from datetime import datetime
+
+
+class Block:
+
+    def __init__(self, index, data, precedent_hash, date):
+        self.precedentHash = precedent_hash
+        self.hash = self.calculate_hash()
+        self.index = index
+        self.data = data
+        self.date = date
+
+    def calculate_hash(self):
+        return sha256(self).hexdigest()
+
+
+class BlockChain:
+
+    def __init__(self):
+        self.blocks = []
+        self.create_first_block()  # The first block doesn't have previous hash
+        self.number_of_block = 0
+
+    def create_first_block(self):
+        first_block = Block(0, "First Block", None, datetime.now())
+        # We can reduce the format if we want to take less space
+        self.add_block(first_block);
+
+    def add_block(self, block):
+        self.blocks[self.number_of_block] = block
+        self.number_of_block += 1
