4a0420f60cf7540f5b2f50be598c4ede7e5c4807
Florian <florian.theault@ecole.ensicaen.fr>
2021-01-17 17:35:33 +0100

    separation "block.py" and "blockchain.py" debuggage BlockChain.py

--- src/Block.py
+++ src/Block.py
@@ -1,8 +1,5 @@
 from hashlib import sha256
 
-from datetime import datetime
-from src.bddBlockChain import *
-
 
 class Block:
 
@@ -24,36 +21,3 @@ class Block:
         return True
 
 
-def init_DB():
-    return DataBaseManager("blockchain.db")
-
-
-class BlockChain:
-
-    def __init__(self):
-        self.blocks = init_DB()
-        self.create_first_block()  # The first block doesn't have previous hash
-
-    def create_first_block(self):
-        first_block = Block(0, "First Block", None, datetime.now())
-        # We can reduce the format if we want to take less space
-        self.add_block(first_block)
-
-    def verify_blockchain(self):
-        curr_index = self.get_last_block().index
-        curr_block = self.get_block_at_index(curr_index)
-        for i in range(curr_index - 1, -1, -1):
-            prev_block = self.get_block_at_index(i)
-            if not prev_block.is_previous(curr_block):
-                return False
-            curr_block = prev_block
-        return True
-
-    def add_block(self, block):
-        self.blocks.add_block(block)
-
-    def get_last_block(self):
-        return self.blocks.getLastBlock()
-
-    def get_block_at_index(self, index):
-        return self.blocks.getBlockAtIndex(index)
--- src/test/testDB.py
+++ src/test/testDB.py
@@ -1,7 +1,15 @@
-from src.Block import *
-import sqlite3
+from datetime import datetime
+
+
 from src.bddBlockChain import *
 
 
 DataBaseManager("testDB").add_block(Block(1,"firstBlock",0,datetime.now()))
+
+
+DataBaseManager("testDB").add_block(Block(2,"secondBlock",0,datetime.now()))
+
+print(DataBaseManager("testDB").getBlockAtIndex(2))
+
+print(DataBaseManager("testDB").getLastBlock())
 
