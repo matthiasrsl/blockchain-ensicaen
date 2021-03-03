f344524deddcb6dc466aa941555aae8cc9abebeb
sipetit <simon.petit@ecole.ensicaen.fr>
2021-03-03 17:30:55 +0100

    QWebEngineView to display visualizer.html added
    Signed-off-by: sipetit <simon.petit@ecole.ensicaen.fr>

--- src/client_gui.py
+++ src/client_gui.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 import src.gui_client
 from src.network import get_local_ip, send_message
@@ -10,6 +10,8 @@ class Client(QtWidgets.QMainWindow, src.gui_client.Ui_MainWindow):
         self.setupUi(self)
         self.sendButton.clicked.connect(self.send_message)
         self.ip = get_local_ip()
+        self.widget.load(
+            QtCore.QUrl("http://localhost:8000/T%C3%A9l%C3%A9chargements/blockchain-visualizer/visualizer.html"))
 
     def send_message(self, message=None):
         if message is not None:
