34cde1116b63faecfd7da9b4ca7f5ecad3e5b042
sipetit <simon.petit@ecole.ensicaen.fr>
2021-03-14 12:08:40 +0100

    Almost working
    Signed-off-by: sipetit <simon.petit@ecole.ensicaen.fr>

--- src/client_terminal.py
+++ src/client_terminal.py
@@ -9,6 +9,9 @@ class Client_terminal:
     def __init__(self, handler):
         self.handler = handler
         self.blockchain = handler.blockchain
+
+        self.hiddenRefreshButton = type('', (object,), {'click': lambda: self.set_displayer_text})
+
         bool = input("Are you first? [y/n]:")
         if bool == "n":
             self.blockchain.blocks.clearDB()
@@ -22,28 +25,30 @@ class Client_terminal:
                 data = input("data:")
                 self.create_block(data)
 
-            elif command == "m" or command == "message":
+            elif command == "me" or command == "message":
                 data = input("message:")
                 self.send_message(data)
 
-            elif command == "m_ip" or command == "message_to_ip":
+            elif command == "me_ip" or command == "message_to_ip":
                 ip = input("ip:")
                 data = input("message:")
                 self.send_message_to_ip(data, ip)
 
+            elif command == "ma" or command == "manual":
+                self.check_receive()
+
             elif command == "exit":
                 exit()
 
-            elif command == "h" or command == "help" :
+            elif command == "h" or command == "help":
                 print("You can use:\n"
                       "- create or c to create a block\n"
-                      "- m or message to send a message to all\n"
-                      "- m_ip or message_to_ip to send a message to a specific ip\n"
+                      "- me or message to send a message to all\n"
+                      "- me_ip or message_to_ip to send a message to a specific ip\n"
                       "- exit to quit")
 
             else:
                 print("command not recognised, use h or help.")
-
 
     def send_message(self, data):
         message = "****"
@@ -65,4 +70,9 @@ class Client_terminal:
         message += mes
         send_message(ip, message)
 
+    def check_receive(self):
+        self.handler.manual_validation = self.handler.manual_validation == False  # toggle
+        print(f'Manual is enabled:{self.handler.manual_validation}')
 
+    def set_displayer_text(self):
+        print(str(self.handler.block_to_add))
